f42a7df21d5f6554c0438e50af534a46ae72068d
Ken Harkey <kennyh7279@gmail.com>
2021-04-22 14:55:15 -0700

    forgot second `>` in cat which accidentially replaced bashrc instead of adding to it

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cp $HOME/.bashrc $HOME/.bashrc.old
 ln -rs .shell-personal $HOME
 
 ## Have bash source the new personal file
-cat >$HOME/.bashrc <<EOL
+cat >>$HOME/.bashrc <<EOL
 # source personal shell settings
 if [ -f $HOME/.shell-personal ]; then
 	. $HOME/.shell-personal
